e341e6d2fe6115c5560bb6bc08a4c9bbfaecdb78
Cameron Bertie <72890283+cameronbertie@users.noreply.github.com>
2022-11-17 21:57:32 +0000

    Built the WebcamCapture component (1/2)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import WebcamCapture from "./WebcamCapture";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-     <h1>Let's Build Snapchat</h1>
+      <BrowserRouter>
+        <div className="app__body">
+          <Routes>
+          <Route path="/test" element={<h1>This is a test</h1>}></Route>
+            <Route path="/" element={<WebcamCapture />}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/WebcamCapture.js
@@ -0,0 +1,46 @@
+import React, { useCallback, useRef } from "react";
+import Webcam from "react-webcam";
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+import { useDispatch } from "react-redux";
+import { setCameraImage } from "./features/cameraSlice";
+
+const videoConstraints = {
+  width: 250,
+  height: 400,
+  facingMode: "user",
+};
+
+function WebcamCapture() {
+  const webcamRef = useRef(null);
+  const dispatch = useDispatch();
+
+
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    dispatch(setCameraImage(imageSrc));
+
+  }, [webcamRef, dispatch]);
+
+  return (
+    <div className="webcamCapture">
+      <Webcam
+        audio={false}
+        height={videoConstraints.height}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={videoConstraints.width}
+        videoConstraints={videoConstraints}
+      />
+
+      <RadioButtonUncheckedIcon 
+      className="webcamCapture"
+      onClick={capture}
+      fontSize="large"
+      />
+
+
+    </div>
+  );
+}
+
+export default WebcamCapture;
